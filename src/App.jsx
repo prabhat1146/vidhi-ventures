bcf17fbcd35e028eeda8ca340d6386c032e9b86c
prabhat kumar <prabhat9468@gmail.com>
2025-09-18 22:14:36 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -319,7 +319,7 @@ export default function App() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen mt-12 bg-gray-100 p-6 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="min-h-screen mt-12 bg-gray-100 p-6 grid gap-2 sm:grid-cols-2 lg:grid-cols-4">
         {products.map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
